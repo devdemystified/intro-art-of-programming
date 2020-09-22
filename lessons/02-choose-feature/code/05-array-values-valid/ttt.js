a5b72c29a69f3fa577517a8c4027d72951090127
Elias Carlston <elias@eliascarlston.com>
2020-09-21 21:38:12 -0400

    move examples from 03 to 02

--- lessons/02-choose-feature/code/05-array-values-valid/ttt.js
+++ lessons/02-choose-feature/code/05-array-values-valid/ttt.js
@@ -1,7 +1,7 @@
 function loadBoard(board) {
   if (!(board instanceof Array)) throw "board must be an array";
   board.forEach(e => {
-   if ([1, 2].indexOf(e) > -1) { return; }
+   if ([1, 2].indexOf(e) > -1) return;
    throw Error("board values must be player numbers");
   });
   this.board = board;
